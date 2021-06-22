150c065a986f834320d3f088572f3ee42013813d
Nick Watson <67918013+ngwatso@users.noreply.github.com>
2021-06-22 01:24:58 +0000

    Added problem #2

--- main.py
+++ main.py
@@ -32,3 +32,53 @@ class Solution:
 
 # ===============
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    from collections import deque
+    
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        
+        queue = deque()
+        
+        self.increasingBSTHelper(root, queue)
+
+        res = self.increasingBSTBuilder(queue)
+        
+        return res
+        
+        
+    def increasingBSTHelper(self, root, queue):
+        
+        if root == None:
+            return
+        
+        if root.left:
+            self.increasingBSTHelper(root.left, queue)
+            
+        queue.append(root.val)
+        
+        if root.right:
+            self.increasingBSTHelper(root.right, queue)
+            
+            
+    def increasingBSTBuilder(self, queue):
+        
+        currNode = TreeNode(queue[0])
+        
+        res = currNode
+        
+        for i in range(len(queue)):
+            if i != 0:
+                res.right = TreeNode(queue[i])
+                res = res.right
+                
+        return currNode
+
+# ===============
+
